afb0bfc5ecc626f9b8a1b4de11d3c311c448aaaa
root <root@A5-004006.localdomain>
2019-01-23 16:34:42 -0700

    CRUD-todolist

---
+++ app.js
@@ -0,0 +1,56 @@
+function newElement () {
+    var li = document.createElement("li")
+    var inputValue = document.getElementById("myInput").value
+    var t = document.createTextNode(inputValue)
+
+    li.appendChild(t)
+
+    if (inputValue ==='') {
+        alert('Please fill out the form')
+    } else {
+        document.getElementById('myUL').appendChild(li)
+    }
+
+    document.getElementById('myInput').value = ""
+
+    var span = document.createElement("SPAN")
+    var txt = document.createTextNode("\u00D7")
+    span.className = 'close' 
+    span.appendChild(txt)
+    li.appendChild(span)
+
+    for( i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+        var div = this.parentElement;
+        //div.style.display = 'none';
+        div.parentElement.removeChild(div)
+        }
+    }
+}
+
+//delete
+var close = document.getElementsByClassName('close')
+var i
+for ( i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = 'none';
+    }
+}
+
+var todos = document.getElementsByTagName("LI");
+var i
+for (i = 0; i < todos.length; i++) {
+    var span = document.createElement('SPAN')
+    span.className = 'close'
+    todos[i].appendChild(span)
+}
+
+//update
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+}, false);
